7bdd2706b7e2dffa289f527cad881720b7fcdcf0
Quentin Chevance <qchevance@ccmbenchmark.com>
2018-08-09 17:10:48 +0200

    fix localstorage

--- booksmarks/src/App.js
+++ booksmarks/src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
 		super(props);
 		this.updateLinks = this.updateLinks.bind(this);
 		this.state = {
-			links: JSON.parse(localStorage.getItem('links')).length > 0 ? JSON.parse(localStorage.getItem('links')) : []
+			links: JSON.parse(localStorage.getItem('links')) !== null ? JSON.parse(localStorage.getItem('links')) : []
 		};
 	}
 
